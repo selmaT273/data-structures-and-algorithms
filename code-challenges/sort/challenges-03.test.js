65f606038bbb6e173e9607883acdec5d035a8ec8
Stacey Teltser <stacey.teltser@gmail.com>
2020-04-01 17:44:22 -0500

    challenge 4 test passing

--- code-challenges/sort/challenges-03.test.js
+++ code-challenges/sort/challenges-03.test.js
@@ -53,6 +53,16 @@ For example, ['Alphabet', 'alphabet', 'carrot', 'Zebra'] is correctly sorted, an
 
 const alphabetizeBetter = (arr) => {
   // Solution code here...
+  arr.sort(function (a, b) {
+    if ( a.toLowerCase() < b.toLowerCase() ) {
+      return -1;
+    } else if ( a.toLowerCase() > b.toLowerCase() ) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
