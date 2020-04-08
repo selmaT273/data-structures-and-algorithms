ca552f2d3e77c4f5fe78e74ecdd3d1242222289c
Stacey Teltser <stacey.teltser@gmail.com>
2020-04-07 23:01:29 -0500

    challenge 2 passing

--- code-challenges/map/challenges-07.test.js
+++ code-challenges/map/challenges-07.test.js
@@ -28,6 +28,11 @@ Write a function named forEachTwoToThe that produces the same output as your for
 
 const forEachTwoToThe = (arr) => {
   // Solution code here...
+  const newArr = [];
+  arr.forEach(element => {
+    newArr.push(Math.pow(2, element));
+  })
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
