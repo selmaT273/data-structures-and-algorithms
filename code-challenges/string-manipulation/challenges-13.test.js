1ad31628904ad7853b6446862c6328477b4ad280
Stacey Teltser <stacey.teltser@gmail.com>
2020-04-15 22:05:18 -0500

    challenge 8 test passing

--- code-challenges/string-manipulation/challenges-13.test.js
+++ code-challenges/string-manipulation/challenges-13.test.js
@@ -121,6 +121,7 @@ For example, [['Brook Testing', 'Actual Person'], ['Human Person', 'Brook again'
 
 const unenrollBrook = (arr) => {
   // Solution code here...
+  return arr.map(classes => classes.filter(eachPerson => !eachPerson.includes('Brook')));
 };
 
 /* ------------------------------------------------------------------------------------------------
